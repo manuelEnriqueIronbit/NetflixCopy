43a264fe4659ca10fed25bc29cbc53ca64c55367
Manuel <enrique.garcia@ironbit.com.mx>
2019-12-02 13:57:55 -0600

    Funcionalidad de seleccion de perfil y avances en plantilla de edicion de perfil

---
+++ Netflix copy/js/app.js
@@ -0,0 +1,12 @@
+let bodyElement = document.body;
+let profiles = document.getElementsByClassName('to-movies-now');
+let profileSelection = document.getElementsByClassName('select-user');
+let profileList = document.getElementById('users-list');
+
+/*window.onload = function () { 
+    document.body.style.overflowY = "hidden"; 
+}*/
+
+let changeBody = () => {
+    bodyElement.style.display = 'auto';
+}
